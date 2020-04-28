fd2ca940ed38ca945eebad58cba899ece47d8313
Jerimiah Willhite <j.willhite@hotmail.com>
2020-04-27 21:11:59 -0700

    switch mapmaking to Folio, change flask's map route

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,6 +1,6 @@
 from decouple import config
 from flask import Flask, render_template, request
-from functions import latlong_finder
+import folium
 
 
 def create_app():
@@ -14,7 +14,18 @@ def create_app():
 
     @app.route('/map', methods=['POST', 'GET'])
     def map():
+        usgs_month_data = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson'
+        m = folium.Map(
+            location=[-59.1759, -11.6016],
+            tiles='Stamen Terrain',
+            zoom_start=2  # Limited levels of zoom for free Mapbox tiles.
+            )
 
+        folium.GeoJson(
+            usgs_month_data,
+            name='geojson'
+            ).add_to(m)
+        m.save('templates/map.html')
         return render_template('map.html', title='Map data got!')
 
     return app
