ec555a408c248dcd1bd4c2d4d43e8d7a2023c9dd
Jerimiah Willhite <j.willhite@hotmail.com>
2020-05-26 10:35:07 -0700

    create exception handling for NotNullViolation

--- flask_app/app.py
+++ flask_app/app.py
@@ -74,6 +74,11 @@ def create_app():
                         time=entry['properties']['time'])
                         db.session.add(quake_entry)
 
+                    except exc.DBAPIError as ex:
+                        #NotNullViolation error
+                        if ex.orig.pgcode == '23502':
+                            continue
+
                     except Exception as e:
                         # prints message with the entry id if something goes wrong
                         print(f"Oh no {e} on {entry['id']}!")
